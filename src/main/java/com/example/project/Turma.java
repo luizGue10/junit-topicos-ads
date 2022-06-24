80041b0ca5735c6042f11ac54d5dda8540ddd06a
luizGue10 <luizeduardoguedes82@gmail.com>
2022-06-24 09:08:59 -0300

    Entrega

--- src/main/java/com/example/project/Turma.java
+++ src/main/java/com/example/project/Turma.java
@@ -1,20 +1,43 @@
 package com.example.project;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Turma {
     
+    List<Double> lista = new ArrayList<>();
+    
+    
     public void addNota(double nota) {
-        return;
+        lista.add(nota);
     }
 
     public int getQuantidadeNotas() {
-        return 0;
+        return lista.size();
     }
 
     public double getMediaDasNotas() {
-        return 0.0;
+        Double totalida = 0.0;
+        
+        for (Double nota : lista) {
+            totalida += nota;
+        }
+        return totalida / lista.size();
     }
 
     public int getQuantidadeNotasMeioresQueAMedia() {
-        return 0;
+        Double media = this.getMediaDasNotas();
+      
+        int contador = 0;
+      
+      
+        for (Double nota : lista) {
+            if(nota < media){
+                contador++;
+            }
+        }
+        
+        return contador;
     }
 }
